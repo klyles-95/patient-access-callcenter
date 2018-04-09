1ef9cf7bcf551d294d7d46290110608e8a1e63a1
klyles-ais <30510675+klyles-ais@users.noreply.github.com>
2018-04-09 08:48:31 -0500

    Server refactor

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,24 +1,24 @@
 
-// router.post('/login', async (req, res) => {
-//   let userData = req.body
+router.post('/login', async (req, res) => {
+  let userData = req.body
 
-//   User.findOne({username: userData.username}, (err, user) => {
-//     if (err) {
-//       console.log(err)    
-//     } else {
-//       if (!user) {
-//         res.status(401).send('Invalid Username')
-//       } else 
-//       if ( user.password !== userData.password) {
-//         res.status(401).send('Invalid Password')
-//       } else {
-//         // let payload = {subject: user._id}
-//         // let token = jwt.sign(payload, 'secretKey')
-//         // res.status(200).send({token})
-//         res.status(200).send(user)
-//       }
-//     }
-//   })
-// })
+  User.findOne({username: userData.username}, (err, user) => {
+    if (err) {
+      console.log(err)    
+    } else {
+      if (!user) {
+        res.status(401).send('Invalid Username')
+      } else 
+      if ( user.password !== userData.password) {
+        res.status(401).send('Invalid Password')
+      } else {
+        // let payload = {subject: user._id}
+        // let token = jwt.sign(payload, 'secretKey')
+        // res.status(200).send({token})
+        res.status(200).send(user)
+      }
+    }
+  })
+})
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -14,18 +14,41 @@ const User = require('./models/user')
 
 app.use(bodyParser.json()) 
 
-app.post('/api/login', async(req, res) => {
-    const {username, password} = req.body
-    const res = await User.findOne({username, password})
-    if(!res){
-        //user is incorrect
-        console.log("incorrect details")
-    } else {
-        // make a session and set user to logged in
-        console.log("logging you in....")
-    }
-    console.log(req.body)
+app.get('/', (req, res) => {
+    res.send('From API route')
 })
+
+app.post('/api/login', async (req, res) => {
+    let userData = req.body
+   
+    User.findOne({username: userData.username}, (err, user) => {
+        if (err){
+            console.log(err)
+        } else {
+            if (!user) {
+              res.status(401).send('Invalid Username')
+            } else 
+            if ( user.password !== userData.password) {
+              res.status(401).send('Invalid Password')
+            } else {     
+              res.status(200).send(user)
+            }
+        }
+    })
+
+})
+
+
+    //const resp = await User.findOne({username, password})
+    // if(!resp){
+    //     //user is incorrect
+    //     console.log("incorrect details")
+    // } else {
+    //     // make a session and set user to logged in
+    //     console.log("logging you in....")
+    // }
+    // console.log(req.body)
+
 
 app.listen(PORT, function(){
     console.log("Server running on localhost:" + PORT);
